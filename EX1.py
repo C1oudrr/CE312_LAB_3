c9c8efbccab5f2900befa53e277883054de68fc7
C1oudrr <98571454+C1oudrr@users.noreply.github.com>
2022-11-19 21:42:46 +0700

    Update EX1.py

--- EX1.py
+++ EX1.py
@@ -2,7 +2,7 @@ stack = ['A','B','C','E','F']
 print("FILO : Before")
 for i in range(5):
     print(stack[i])
-stack.pop(0)
+stack.pop()
 print("FILO : After")
 for i in range(4):
     print(stack[i])
