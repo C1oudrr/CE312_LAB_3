ecdd26843352bdd296e45e6cae0f4a42af409be6
C1oudrr <98571454+C1oudrr@users.noreply.github.com>
2022-11-19 21:43:02 +0700

    Update EX2.py

--- EX2.py
+++ EX2.py
@@ -1,19 +1,8 @@
 stack = ['A','B','C','E','F']
-print("Reverse : Before")
+print("FILO : Before")
 for i in range(5):
     print(stack[i])
-stack.pop()
-stack.pop()
-stack.pop()
-stack.pop()
-stack.pop()
-
-stack.append('F')
-stack.append('E')
-stack.append('C')
-stack.append('B')
-stack.append('A')
-
-print("Reverse : After")
-for i in range(5):
+stack.pop(0)
+print("FILO : After")
+for i in range(4):
     print(stack[i])
